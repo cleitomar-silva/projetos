513ccee1b5102b68cf881b917ac2ec0d337d9271
cleiton <cleitomarrodrigues@gmail.com>
2020-08-17 15:52:31 -0300

    acl - laravel
    inicio

---
+++ acl-laravel/app/Http/Controllers/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(Post $post)
+    {
+       // $posts = $post->all();
+
+       $posts = $post->where('user_id', auth()->user()->id)->get();
+
+        return view('home', compact('posts'));
+    }
+
+    public function update($idPost)
+    {
+        $post = Post::find($idPost);
+
+        return view("update-post", compact('post'));
+    }
+
+}
---
+++ acl-laravel/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @forelse ($posts as $post)
+
+        <h1>{{$post->title}}</h1>
+        <p>{{$post->description}}</p>
+        <b>Author: {{ $post->user->name }}</b>
+        <a href="{{ url("/post/$post->id/update") }}">Editar</a>
+        <hr>
+
+    @empty
+
+        <p>Nenhum Post Cadastrado!</p>
+
+    @endforelse
+</div>
+@endsection
